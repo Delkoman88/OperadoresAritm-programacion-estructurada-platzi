eab43667ec545148997796966c156cf3d85902e3
Edgar Mancilla <delkoman@gmail.com>
2020-02-06 13:12:52 -0800

    Fórmulas corregidas, se agregó valor de pi

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#ifndef MPI
+#define M_PI 3.14159265358979323846
+#endif
 //Ingresar los valores necesarios para calcular el area de un cilindro
 int main()
 {
@@ -17,8 +21,8 @@ int main()
 
 
     //Se realiza la operación para calcular área y volumen de cilindro
-    rArea = (2 * 3.1416) * (radio * radio);
-    rVolumen = (rArea * altura);
+    rArea = ((2 * M_PI) * (radio * altura)) + ((2 * M_PI)* pow(radio,2));
+    rVolumen = ((altura * M_PI) * pow(radio,2));
 
     //Se muestran los resultados en pantalla
     printf("El area del cilindro es: %f \n", rArea);
